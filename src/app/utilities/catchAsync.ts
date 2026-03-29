53dac67176a69124676b23fd4111c19cf9af3806
ronyislam502 <ronyislam502@gmail.com>
2026-03-29 23:33:43 +0600

    update

--- src/app/utilities/catchAsync.ts
+++ src/app/utilities/catchAsync.ts
@@ -1,8 +1,18 @@
-import { NextFunction, Request, RequestHandler, Response } from "express";
+// import { NextFunction, Request, RequestHandler, Response } from "express";
 
-const catchAsync = (fn: RequestHandler) => {
-  return (req: Request, res: Response, next: NextFunction) => {
-    Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+// const catchAsync = (fn: RequestHandler) => {
+//   return (req: Request, res: Response, next: NextFunction) => {
+//     Promise.resolve(fn(req, res, next)).catch((error) => next(error));
+//   };
+// };
+
+// export default catchAsync;
+
+import { RequestHandler } from "express";
+
+const catchAsync = (fn: RequestHandler): RequestHandler => {
+  return (req, res, next) => {
+    Promise.resolve(fn(req, res, next)).catch(next);
   };
 };
 
