560a5ae896b998414b21291d0cfb362cb6e34169
ronyislam502 <ronyislam502@gmail.com>
2026-04-01 03:10:00 +0600

    link

--- src/app.ts
+++ src/app.ts
@@ -35,7 +35,7 @@ app.use(express.json());
 app.use(cookieParser());
 app.use(
   cors({
-    origin: [config.client_url as string, "http://localhost:3000"],
+    origin: [config.client_url as string],
     credentials: true,
   })
 );
